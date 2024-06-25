a28d7d69b004ab1d8e4b782f6a2129105d3bed99
chenliangyu <501305528@qq.com>
2024-06-25 16:58:44 +0800

    filter stock code

--- src/rpc_service.cpp
+++ src/rpc_service.cpp
@@ -6,7 +6,6 @@
 
 #include "config.h"
 
-
 namespace cpptdx {
 
 static void CallAfterRpc(brpc::Controller* cntl, const google::protobuf::Message* req, const google::protobuf::Message* res) {
@@ -18,8 +17,8 @@ static void CallAfterRpc(brpc::Controller* cntl, const google::protobuf::Message
     // LOG(INFO) << "req:" << req_str
     //             << " res:" << res_str;
 }
-HttpServiceImpl::HttpServiceImpl() { 
-    tdx_hq_api_ = create_tdx_hq_api(); 
+HttpServiceImpl::HttpServiceImpl() {
+    tdx_hq_api_ = create_tdx_hq_api();
     connect_to_server();
 }
 HttpServiceImpl::~HttpServiceImpl() {}
@@ -30,42 +29,40 @@ void HttpServiceImpl::connect_to_server() {
         if (tdx_hq_api_->connect(host.first, host.second)) {
             // LOINFO() << " connect success!" << "ip:" << host.first << " port:" << host.second;
             break;
-        }else{
-        //    LOINFO() << " connect failed!" << "ip:" << host.first << " port:" << host.second; 
+        } else {
+            //    LOINFO() << " connect failed!" << "ip:" << host.first << " port:" << host.second;
         }
     }
 }
 
-void HttpServiceImpl::GetSecuritySnapshots(google::protobuf::RpcController* cntl_base, const rpc_server::HttpRequest* req, 
-                                            rpc_server::HttpResponse* res, google::protobuf::Closure* done) {
+void HttpServiceImpl::GetSecuritySnapshots(google::protobuf::RpcController* cntl_base, const rpc_server::HttpRequest* req, rpc_server::HttpResponse* res, google::protobuf::Closure* done) {
     // This object helps you to call done->Run() in RAII style. If you need
     // to process the request asynchronously, pass done_guard.release().
     brpc::ClosureGuard done_guard(done);
 
     brpc::Controller* cntl = static_cast<brpc::Controller*>(cntl_base);
     vector<pair<string, Market>> stock_list;
-    
+
     auto array = nlohmann::json::parse(cntl->request_attachment().to_string());
-    if(array.is_array()){
-        for(const auto& item : array.items()){
+    if (array.is_array()) {
+        for (const auto& item : array.items()) {
             string code_str = item.value()["code"];
             string market_str = item.value()["market"];
             Market market = (Market)atoi(market_str.c_str());
-            stock_list.emplace_back(std::make_pair(code_str,market));
+            stock_list.emplace_back(std::make_pair(code_str, market));
         }
     }
     auto snapshots = tdx_hq_api_->get_security_snapshots(stock_list);
 
     cntl->http_response().set_content_type("text/plain");
     nlohmann::json ret_array = nlohmann::json::array();
-    try{
-        for(const auto& snap : snapshots){
+    try {
+        for (const auto& snap : snapshots) {
             nlohmann::json j;
             to_json(j, snap);
             ret_array.emplace_back(j);
         }
     } catch (nlohmann::json::type_error& e) {
-
     };
 
     butil::IOBufBuilder os;
@@ -73,44 +70,56 @@ void HttpServiceImpl::GetSecuritySnapshots(google::protobuf::RpcController* cntl
     os.move_to(cntl->response_attachment());
 }
 
-void HttpServiceImpl::GetSecurityList(google::protobuf::RpcController* cntl_base, const rpc_server::HttpRequest* req, 
-                                        rpc_server::HttpResponse* res, google::protobuf::Closure* done) {
-
+void HttpServiceImpl::GetSecurityList(google::protobuf::RpcController* cntl_base, const rpc_server::HttpRequest* req, rpc_server::HttpResponse* res, google::protobuf::Closure* done) {
     brpc::ClosureGuard done_guard(done);
 
     brpc::Controller* cntl = static_cast<brpc::Controller*>(cntl_base);
     Market market = Market::kSH;
     int start = 0;
     for (brpc::URI::QueryIterator it = cntl->http_request().uri().QueryBegin(); it != cntl->http_request().uri().QueryEnd(); ++it) {
-        if(it->first == "market"){
+        if (it->first == "market") {
             market = (Market)atoi(it->second.c_str());
-        }else if(it->first == "start"){
+        } else if (it->first == "start") {
             start = atoi(it->second.c_str());
         }
     }
-    auto security_infos = tdx_hq_api_->get_security_list(market,start);
+    // if(market == Market::kSH){
+
+    // }else{
+
+    // }
+    nlohmann::json array = nlohmann::json::array();
+    int count = tdx_hq_api_->get_security_count(market);
+    for (int i = 0; i < (count / 1000) + 1; ++i) {
+        auto security_infos = tdx_hq_api_->get_security_list(market, i * 1000);
+        vector<SecurityInfo> filtered_infos;
+        std::copy_if(security_infos.begin(), security_infos.end(), std::back_inserter(filtered_infos), [&](const SecurityInfo& info) {
+            if (market == Market::kSH) {
+                return info.code.size() > 1 && info.code[0] == '6';
+            } else {
+                return info.code.size() > 1 && (0 == strncmp(info.code.c_str(), "00", 2) || 0 == strncmp(info.code.c_str(), "30", 2));
+            }
+        });
+
+        try {
+            for (const auto& info : filtered_infos) {
+                nlohmann::json j;
+                to_json(j, info);
+                array.emplace_back(j);
+            }
+        } catch (nlohmann::json::type_error& e) {
+        };
+    }
+
     // Fill response.
     cntl->http_response().set_content_type("text/plain");
-    
-    nlohmann::json array = nlohmann::json::array();
-    try{
 
-        for(const auto& info : security_infos){
-            nlohmann::json j;
-            to_json(j, info);
-            array.emplace_back(j);
-        }
-    } catch (nlohmann::json::type_error& e) {
-
-    };
     butil::IOBufBuilder os;
     os << array.dump();
     os.move_to(cntl->response_attachment());
 }
 
-void HttpServiceImpl::GetSecurityKlines(google::protobuf::RpcController* cntl_base, const rpc_server::HttpRequest* req, 
-                                        rpc_server::HttpResponse* res, google::protobuf::Closure* done) {
-
+void HttpServiceImpl::GetSecurityKlines(google::protobuf::RpcController* cntl_base, const rpc_server::HttpRequest* req, rpc_server::HttpResponse* res, google::protobuf::Closure* done) {
     brpc::ClosureGuard done_guard(done);
 
     brpc::Controller* cntl = static_cast<brpc::Controller*>(cntl_base);
@@ -120,44 +129,40 @@ void HttpServiceImpl::GetSecurityKlines(google::protobuf::RpcController* cntl_ba
     int start = 0;
     int count = 0;
     for (brpc::URI::QueryIterator it = cntl->http_request().uri().QueryBegin(); it != cntl->http_request().uri().QueryEnd(); ++it) {
-
-        if(it->first == "cat"){
+        if (it->first == "cat") {
             cat = atoi(it->second.c_str());
-        }else if(it->first == "market"){
+        } else if (it->first == "market") {
             market = atoi(it->second.c_str());
-        }else if(it->first == "code"){
+        } else if (it->first == "code") {
             code = it->second;
-        }else if(it->first == "start"){
+        } else if (it->first == "start") {
             start = atoi(it->second.c_str());
-        }else if(it->first == "count"){
+        } else if (it->first == "count") {
             count = atoi(it->second.c_str());
         }
     }
-    auto res_klines = tdx_hq_api_->get_security_klines((Category)cat,(Market)market, code, start, count);
+    auto res_klines = tdx_hq_api_->get_security_klines((Category)cat, (Market)market, code, start, count);
     // Fill response.
     cntl->http_response().set_content_type("text/plain");
 
     nlohmann::json array = nlohmann::json::array();
-    try{
-
-        for(const auto& k : res_klines){
+    try {
+        for (const auto& k : res_klines) {
             nlohmann::json j;
             to_json(j, k);
             array.emplace_back(j);
         }
     } catch (nlohmann::json::type_error& e) {
-
     };
     butil::IOBufBuilder os;
     os << array.dump();
     os.move_to(cntl->response_attachment());
 }
 
-int run_server(){
+int run_server() {
     brpc::Server server;
     HttpServiceImpl http_svc;
-    if (server.AddService(&http_svc,
-                          brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
+    if (server.AddService(&http_svc, brpc::SERVER_DOESNT_OWN_SERVICE) != 0) {
         LOG(ERROR) << "Fail to add http_svc";
         return -1;
     }
